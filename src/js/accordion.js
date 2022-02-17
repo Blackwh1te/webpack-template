a129367f2606255a399cf803d4dd9baa2499af73
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2022-02-17 11:11:46 +0500

    Code refactoring.

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,8 +1,9 @@
-import Collection from './generic/collection';
+import Collection from './generic/collection'
 import {getCfg} from './utils/getCfg'
 import {onAjaxContentLoaded} from './generic/eventing'
+import {clearAnimationsArray} from './utils/clearAnimationsArray'
 
-export const instance = '[data-js-accordion]';
+export const instance = '[data-js-accordion]'
 
 export class Accordion {
   els = {
@@ -11,12 +12,12 @@ export class Accordion {
     summary: '[data-js-accordion-summary]',
     content: '[data-js-accordion-content]',
     btn: '[data-js-accordion-btn]',
-  };
+  }
 
   stateClasses = {
     isOpen: 'is-open',
     isFullyExpanded: 'is-fully-expanded',
-  };
+  }
 
   defaultCfg = {
     isAutoClosing: true,
@@ -25,96 +26,90 @@ export class Accordion {
       duration: 500,
       easing: 'ease',
     },
-  };
+  }
 
   constructor(instance) {
-    this.instance = instance;
-    this.items = this.instance.querySelectorAll(`:scope > ${this.els.item}`);
-    this.summaryEls = this.instance.querySelectorAll(`:scope > ${this.els.item} > ${this.els.summary}`);
-    this.cfg = getCfg(this.instance, this.els.instance, this.defaultCfg);
+    this.instance = instance
+    this.items = this.instance.querySelectorAll(`:scope > ${this.els.item}`)
+    this.summaryEls = this.instance.querySelectorAll(`:scope > ${this.els.item} > ${this.els.summary}`)
+    this.cfg = getCfg(this.instance, this.els.instance, this.defaultCfg)
     this.clickableEls = this.getClickableEls()
-    this.init();
-    this.bindEvents();
+    this.init()
+    this.bindEvents()
   }
 
   getClickableEls() {
     if (this.cfg.wholeSummaryClickable || this.cfg.isExtraView) {
       return this.summaryEls
     } else {
-      return this.instance.querySelectorAll(`:scope > ${this.els.item} > ${this.els.summary} ${this.els.btn}`);
+      return this.instance.querySelectorAll(`:scope > ${this.els.item} > ${this.els.summary} ${this.els.btn}`)
     }
   }
 
-  clearAnimationsArray(arr) {
-    arr.forEach((animation) => {
-      animation.cancel();
-    });
-  }
-
-  isOpen = (item) => item.classList.contains(this.stateClasses.isOpen);
+  isOpen = (item) => item.classList.contains(this.stateClasses.isOpen)
 
   open(item) {
-    const content = item.querySelector(this.els.content);
+    const content = item.querySelector(this.els.content)
 
-    this.clearAnimationsArray(content.getAnimations());
-    item.open = true;
-    item.classList.add(this.stateClasses.isOpen);
+    clearAnimationsArray(content.getAnimations())
+    item.open = true
+    item.classList.add(this.stateClasses.isOpen)
 
     content.animate([
       {height: '0'},
       {height: `${content.scrollHeight}px`},
     ], this.cfg.animationParams).onfinish = () => {
-      item.classList.add(this.stateClasses.isFullyExpanded);
-    };
+      item.classList.add(this.stateClasses.isFullyExpanded)
+    }
 
     if (this.cfg.isAutoClosing) {
-      this.closeAllExcludeOne(item);
+      this.closeAllExcludeOne(item)
     }
   }
 
   close(item) {
-    const content = item.querySelector(this.els.content);
+    const content = item.querySelector(this.els.content)
 
-    this.clearAnimationsArray(content.getAnimations());
-    item.classList.remove(this.stateClasses.isOpen);
-    item.classList.remove(this.stateClasses.isFullyExpanded);
+    clearAnimationsArray(content.getAnimations())
+    item.classList.remove(this.stateClasses.isOpen)
+    item.classList.remove(this.stateClasses.isFullyExpanded)
     content.animate([
       {height: `${content.scrollHeight}px`},
       {height: '0'},
     ], this.cfg.animationParams).onfinish = () => {
-      item.open = false;
-    };
+      item.open = false
+    }
   }
 
   closeAllExcludeOne(excludeEl) {
     this.items.forEach((item) => {
       if (item !== excludeEl) {
-        this.close(item);
+        this.close(item)
       }
-    });
+    })
   }
 
   handleSummaryClick(e) {
     if (e.target instanceof HTMLAnchorElement) {
-      return;
+      return
     }
-    e.preventDefault();
+    e.preventDefault()
   }
 
   handleClickableElClick(e) {
-    const item = e.target.closest(this.els.item);
+    const item = e.target.closest(this.els.item)
 
     this.isOpen(item) ?
       this.close(item) :
-      this.open(item);
+      this.open(item)
   }
 
   setInitialFullyExpandedState() {
     this.items.forEach(item => {
       if (this.isOpen(item)) {
-        item.classList.add(this.stateClasses.isFullyExpanded);
+        item.classList.add(this.stateClasses.isFullyExpanded)
       }
-    });
+    })
   }
 
   init() {
@@ -123,30 +118,30 @@ export class Accordion {
 
   bindEvents() {
     this.summaryEls.forEach((summary) => {
-      summary.addEventListener('click', (e) => this.handleSummaryClick(e));
-    });
+      summary.addEventListener('click', (e) => this.handleSummaryClick(e))
+    })
     this.clickableEls.forEach((clickableEl) => {
-      clickableEl.addEventListener('click', (e) => this.handleClickableElClick(e));
-    });
+      clickableEl.addEventListener('click', (e) => this.handleClickableElClick(e))
+    })
   }
 }
 
 export class AccordionCollection extends Collection {
   constructor() {
-    super(instance, Accordion);
-    this.init();
+    super(instance, Accordion)
+    this.init()
     this.bindEvents()
   }
 
   init(context = document) {
     context.querySelectorAll(instance).forEach((el) => {
-      this.collection = new Accordion(el);
-    });
+      this.collection = new Accordion(el)
+    })
   }
 
   bindEvents() {
     onAjaxContentLoaded((e) => {
-      this.init(e.detail.content);
-    });
+      this.init(e.detail.content)
+    })
   }
 }
